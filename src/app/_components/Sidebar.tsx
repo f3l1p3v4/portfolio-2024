35d655039215812ad3986a0ad9d41a03a3a9ee42
f3l1p3v4 <fvaldez018@gmail.com>
2025-12-04 15:17:52 -0400

    added new link  whatsapp

--- src/app/_components/Sidebar.tsx
+++ src/app/_components/Sidebar.tsx
@@ -40,7 +40,7 @@ export function Sidebar() {
             digitais modernas e{' '}
             <span className={styles.infoDescriptionColor}>eficientes</span>.
           </span>
-          <Link href="https://wa.me/5567992656801?text=Ol%C3%A1%20estou%20vindo%20do%20seu%20site!" className={styles.infoContact}>
+          <Link href="https://wa.me/5567992656801?text=Ol%C3%A1!%20Encontrei%20seu%20contato%20atrav%C3%A9s%20do%20seu%20site." className={styles.infoContact}>
             <FaWhatsapp />  
             Entrar em contato
           </Link>
